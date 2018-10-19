593c57192de338298c5f072df864ad60e1c3aaa7
Jihong Zhang <jihong.zhang@ku.edu>
2018-10-19 09:30:09 -0500

    add new things

--- R/write_mplus_csv.R
+++ R/write_mplus_csv.R
@@ -1,10 +1,12 @@
-#' Write CSV file for Mplus Function
+#' Save CSV file compatible for Mplus
 #'
-#' This function allows you to save file compatible for MPlus
-#' @param data a dataframe
-#' @param file the filename you want to save
+#' \code{write_mplus_csv} allows you to save file compatible for MPlus. Specifically,
+#' headers will be removed.
+#' @param data A dataframe stored in R.
+#' @param file Tha abosulte path of saved file.
 #' @keywords mplus csv
 #' @export
+#' @seealso This function wraps up \code{\link[utils]{write.table}}.
 #' @examples
 #' write_mplus_csv(mtcars, file="~/Desktop/mtcars.csv")
 #'
@@ -12,3 +14,4 @@
 write_mplus_csv <- function(data, file) {
   write.table(data, file = file, sep=",", col.names = FALSE)
 }
+
