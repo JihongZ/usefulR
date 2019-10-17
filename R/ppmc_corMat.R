93d51cf40d96ca2a9c1384831c8977b8fcac1291
JihongZ <jihong-zhang@uiowa.edu>
2019-10-16 20:59:16 -0500

    add rev.link

---
+++ R/ppmc_corMat.R
@@ -0,0 +1,16 @@
+#' PPMC graphic matrix for correlation matrix
+#'
+#' This function allows you to get fixed scaling correction factor HO for lavaan model to mimic Mplus
+#' @param simData.list a list including all simulated data
+#' @param data the original data
+#' @keywords lavaan scale_factor
+#' @export
+#' @examples
+#' simData.list <- ppmc_simData(model.Posterior.all, iternum)
+#' ppmc_corMat(simData.list, data)
+#'
+#'
+
+ppmc_corMat <- function(simData.list, data) {
+
+}
---
+++ R/rev.link.R
@@ -0,0 +1,12 @@
+#' Inverse Link Function for logistic regression
+#'
+#' @description  This function automate calculating reverse link function value for logistic regression
+#' @param logofOdds log of odds ratio
+#' @author Jihong Zhang
+#' @keywords Link Function
+#' @export
+#' @examples
+
+rev.link <- function(logofOdds) {
+  mu = exp(logofOdds) / (1 + exp(logofOdds))
+}
