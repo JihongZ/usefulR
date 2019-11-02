e142cddbafab063dd9ce13a1004c2b9c08b0a283
JihongZ <jihong-zhang@uiowa.edu>
2019-11-01 21:22:14 -0500

    check2

---
+++ R/MCMCsummary.R
@@ -0,0 +1,17 @@
+#' The cleanest summary for mcmc.list object
+#'
+#' A function to report the summary table of mcmc object
+#' @param mcmcObj mcmc.list object
+#' @export
+#'
+#'
+
+MCMCsummary <- function(mcmcObj) {
+
+  stopifnot(class(model01a) == "mcmc.list")
+  res = data.frame(
+    cbind(round(summary(mcmcObj)$statistics[,1:2], 4),    # Posterior Mean and SD
+          round(summary(mcmcObj)$quantiles[,c(1,5)], 4)), # Posterior Percentile 2.5% and 97.5%
+    Rhat = gelman.diag(mcmcObj, multivariate = FALSE)[[1]][,1], check.names = FALSE)
+
+}
