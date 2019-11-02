84e349059d587881884c978d3d57dcc697ccd15d
JihongZ <jihong-zhang@uiowa.edu>
2019-11-01 21:24:18 -0500

    revise MCMCsummary

--- R/MCMCsummary.R
+++ R/MCMCsummary.R
@@ -13,5 +13,5 @@ MCMCsummary <- function(mcmcObj) {
     cbind(round(summary(mcmcObj)$statistics[,1:2], 4),    # Posterior Mean and SD
           round(summary(mcmcObj)$quantiles[,c(1,5)], 4)), # Posterior Percentile 2.5% and 97.5%
     Rhat = gelman.diag(mcmcObj, multivariate = FALSE)[[1]][,1], check.names = FALSE)
-
+  return(res)
 }
